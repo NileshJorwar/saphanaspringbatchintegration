3de9a6188b18d201fc2d6a6f117ab7eefa3a4cdc
nileshjorwar <jorwarnilesh@gmail.com>
2021-11-07 12:01:51 -0600

    added scripts for runtime during startup

--- src/main/java/com/sap/batch/saphanaspringbatchintegration/SaphanaspringbatchintegrationApplication.java
+++ src/main/java/com/sap/batch/saphanaspringbatchintegration/SaphanaspringbatchintegrationApplication.java
@@ -1,18 +1,24 @@
 package com.sap.batch.saphanaspringbatchintegration;
 
+import com.sap.batch.saphanaspringbatchintegration.loader.DbLoaderImplementation;
+import com.sap.batch.saphanaspringbatchintegration.loader.IDbLoader;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.integration.config.annotation.EnableBatchIntegration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(exclude = BatchAutoConfiguration.class)
 @EnableBatchProcessing
 @EnableBatchIntegration
 public class SaphanaspringbatchintegrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SaphanaspringbatchintegrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                SpringApplication.run(SaphanaspringbatchintegrationApplication.class, args);
+        IDbLoader dbLoader = applicationContext.getBean(DbLoaderImplementation.class);
+        dbLoader.readyDatabase();
+    }
 
 }
